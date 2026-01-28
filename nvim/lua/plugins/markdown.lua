1d89ba40eb95c55dd38f16c1beaca4adb62dfa03
Seungmin Wang <smwang@rebellions.ai>
2026-01-28 09:30:24 +0900

    feat: Add render-markdown
    Signed-off-by: Seungmin Wang <smwang@rebellions.ai>

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,12 +1,33 @@
 return {
-	"iamcco/markdown-preview.nvim",
-	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-	build = "cd app && npm install && git checkout -- .",
-	init = function()
-		vim.g.mkdp_filetypes = { "markdown" }
-	end,
-	ft = { "markdown" },
-	keys = {
-		{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
+	-- markdown-preview: based on browser
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install && git checkout -- .",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+		keys = {
+			{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview (Browser)" },
+		},
+	},
+	-- render-markdown: based on buffer
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		ft = { "markdown", "quarto" },
+		opts = {
+			enabled = false,
+			admonition = {
+				enabled = true,
+			},
+			pipe_table = {
+				preset = "round",
+			},
+		},
+		keys = {
+			{ "<leader>mr", "<cmd>RenderMarkdown toggle<cr>", desc = "Render Markdown (Buffer)" },
+		},
 	},
 }
