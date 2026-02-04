f93a8dd1acd0d89eac20d12c06bc42630f97dd35
Seungmin Wang <smwang@rebellions.ai>
2026-02-05 08:16:48 +0900

    feat: Add expandtab toggling at F6
    Signed-off-by: Seungmin Wang <smwang@rebellions.ai>

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,11 +3,24 @@ vim.keymap.set("n", "<F2>", "gT", { noremap = true, silent = true, desc = "Previ
 vim.keymap.set("n", "<F3>", "gt", { noremap = true, silent = true, desc = "Next tab", })
 vim.keymap.set("n", "<F9>", ":tabnew<CR>", { noremap = true, silent = true, desc = "Open new tab", })
 
+-- Toggle expandtab (Tabs vs Spaces)
+vim.keymap.set("n", "<F6>", function()
+	vim.opt.expandtab = not vim.opt.expandtab:get()
+
+	local mode = vim.opt.expandtab:get() and "Spaces" or "Tabs"
+
+	vim.notify("Indent Mode: " .. mode, vim.log.levels.INFO, { title = "Indentation" })
+end, {
+		noremap = true,
+		silent = true,
+		desc = "Toggle expandtab (Tabs/Spaces)",
+	})
 -- Indentation settings (4 spaces)
 vim.keymap.set("n", "<F7>", function()
 	vim.opt.tabstop = 4
 	vim.opt.shiftwidth = 4
 	-- vim.opt.expandtab = true
+	vim.notify("Tab Size: 4", vim.log.levels.INFO, { title = "Indentation" })
 end, {
 		noremap = true,
 		silent = true,
@@ -18,6 +31,7 @@ vim.keymap.set("n", "<F8>", function()
 	vim.opt.tabstop = 8
 	vim.opt.shiftwidth = 8
 	-- vim.opt.expandtab = true
+	vim.notify("Tab Size: 8", vim.log.levels.INFO, { title = "Indentation" })
 end, {
 		noremap = true,
 		silent = true,
