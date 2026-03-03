4045fe08a801564b0fa870ee8f941e948dc7ae23
Seungmin Wang <smwang@rebellions.ai>
2026-03-03 09:28:20 +0900

    refactor: Reorganize telescope.lua structure and add ui-select dropdown theme
    - Move telescope core setup (defaults, pickers) into telescope.nvim spec
    - Move keymaps after setup() call in telescope.nvim spec
    - Separate telescope-ui-select.nvim into its own clean spec
    - Add dropdown theme for vim.ui.select via extensions config
    - Enable load_extension for ui-select

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,16 +3,6 @@ return {
 		"nvim-telescope/telescope.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set( "n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
-			vim.keymap.set( "n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
-			vim.keymap.set( "n", "<leader>fb", builtin.buffers, { desc = "Telescope: Buffers" })
-			vim.keymap.set( "n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
 			require("telescope").setup({
 				defaults = {
 					layout_strategy = 'vertical',
@@ -30,7 +20,6 @@ return {
 						find_command = {
 							'fd', '--type', 'f', '-H', '--strip-cwd-prefix',
 						},
-						--						theme = "dropdown",
 						preview_title = "File Preview",
 					},
 					live_grep = {
@@ -41,6 +30,25 @@ return {
 					},
 				},
 			})
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
 		end,
 	},
 }
