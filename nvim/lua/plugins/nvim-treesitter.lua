afb43fdb83070d58721274916c107eb6fa1d4a56
Seungmin Wang <smwang@rebellions.ai>
2026-04-06 11:41:22 +0900

    fix: Migrate nvim-treesitter to main branch for Neovim 0.12 compat
    The archived master branch uses nvim_treesitter#indent() which is
    incompatible with Neovim 0.12's treesitter API changes. Switch to
    the main branch which requires tree-sitter CLI and uses the new
    module API for parser installation and indentation.
    
    Signed-off-by: Seungmin Wang <smwang@rebellions.ai>

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,39 +1,53 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	build = ":TSUpdate",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
+		require("nvim-treesitter").setup()
 
-		configs.setup({
-			ensure_installed = {
-				"asm",
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"javascript",
-				"jsdoc",
-				"json",
-				"jsonc",
-				"lua",
-				"luadoc",
-				"luap",
-				"markdown",
-				"markdown_inline",
-				"printf",
-				"python",
-				"query",
-				"regex",
-				"toml",
-				"tsx",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"xml",
-				"yaml" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-			sync_install = false,
-			highlight = { enable = true },
-			indent = { enable = true },
+		-- Install parsers
+		local ensure_installed = {
+			"asm",
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"jsonc",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"printf",
+			"python",
+			"query",
+			"regex",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"xml",
+			"yaml",
+		}
+		local installed = require("nvim-treesitter").get_installed()
+		local to_install = vim.tbl_filter(function(lang)
+			return not vim.list_contains(installed, lang)
+		end, ensure_installed)
+		if #to_install > 0 then
+			require("nvim-treesitter").install(to_install)
+		end
+
+		-- Treesitter indent
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				if pcall(vim.treesitter.get_parser) then
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end
+			end,
 		})
-	end
+	end,
 }
