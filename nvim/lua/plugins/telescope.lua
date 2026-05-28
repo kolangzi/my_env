bae05126525ce47d7d8d9d32debe59ddcd0fda4f
Seungmin Wang <smwang@rebellions.ai>
2026-05-28 09:10:38 +0900

    fix: Merge telescope setup calls into single spec
    - Combine telescope.nvim and telescope-ui-select.nvim into one lazy
      spec so setup() is only called once
    - Previously two separate setup() calls existed; the second one
      overwrote defaults/pickers and only survived because of lazy load
      ordering, risking silent loss of find_files/live_grep config when
      the order changed
    - Move telescope-ui-select.nvim to dependencies to guarantee
      availability before setup
    
    Signed-off-by: Seungmin Wang <smwang@rebellions.ai>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,13 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope-ui-select.nvim",
+		},
 		config = function()
-			require("telescope").setup({
+			local telescope = require("telescope")
+			telescope.setup({
 				defaults = {
 					layout_strategy = 'vertical',
 					layout_config = {
@@ -29,26 +33,19 @@ return {
 						end,
 					},
 				},
-			})
-
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
-			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Buffers" })
-			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_cursor({}),
 					},
 				},
 			})
-			require("telescope").load_extension("ui-select")
+			telescope.load_extension("ui-select")
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope: Find files" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope: Live grep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope: Buffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope: Help tags" })
 		end,
 	},
 }
